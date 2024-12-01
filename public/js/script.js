4c294ffe79502e7bbf8df881c44abf06d07496b4
Panca16 <672021010@student.uksw.edu>
2024-12-01 19:46:29 +0700

    Merubah FE menjadi responvie

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,46 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerMenu = document.querySelector(".hamburger-menu");
+  const mobileNav = document.querySelector(".mobile-nav");
+
+  if (hamburgerMenu && mobileNav) {
+    hamburgerMenu.addEventListener("click", function () {
+      this.classList.toggle("active");
+      mobileNav.classList.toggle("active");
+    });
+
+    // Menutup menu saat mengklik di luar menu
+    document.addEventListener("click", function (event) {
+      if (
+        !hamburgerMenu.contains(event.target) &&
+        !mobileNav.contains(event.target)
+      ) {
+        hamburgerMenu.classList.remove("active");
+        mobileNav.classList.remove("active");
+      }
+    });
+  }
+  // Fungsi untuk menu toggle
+  const menuToggle = document.querySelector(".menu-toggle");
+  const nav = document.querySelector("nav");
+
+  if (menuToggle && nav) {
+    menuToggle.addEventListener("click", function () {
+      menuToggle.classList.toggle("active");
+      nav.classList.toggle("active");
+      document.body.classList.toggle("menu-open");
+    });
+
+    // Menutup menu saat link diklik
+    const navLinks = nav.querySelectorAll("a");
+    navLinks.forEach((link) => {
+      link.addEventListener("click", () => {
+        menuToggle.classList.remove("active");
+        nav.classList.remove("active");
+        document.body.classList.remove("menu-open");
+      });
+    });
+  }
+
   // Fungsi untuk menginisialisasi carousel
   function initializeCarousel(containerSelector) {
     const container = document.querySelector(containerSelector);
